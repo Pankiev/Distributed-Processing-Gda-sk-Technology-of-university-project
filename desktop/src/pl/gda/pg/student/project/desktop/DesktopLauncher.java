8ad8fc6e1203bb6e770172d19e6774238989ff3f
Adrian Pankiewicz <farami11@tlen.pl>
2017-02-24 19:29:39 +0100

    Project init

---
+++ desktop/src/pl/gda/pg/student/project/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.gda.pg.student.project.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.gda.pg.student.project.ProjectGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectGame(), config);
+	}
+}
