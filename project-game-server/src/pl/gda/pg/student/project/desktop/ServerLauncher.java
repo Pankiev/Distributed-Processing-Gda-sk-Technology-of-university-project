5db4e175382a345ba7e68a778de8db3dfc29353d
Adrian Pankiewicz <farami11@tlen.pl>
2017-02-24 22:08:49 +0100

    Separate server launcher project added

---
+++ project-game-server/src/pl/gda/pg/student/project/desktop/ServerLauncher.java
@@ -0,0 +1,15 @@
+package pl.gda.pg.student.project.desktop;
+
+public class ServerLauncher
+{
+	public static void main(String[] arg)
+	{
+		// Here is where we start server
+		// If start it as libgdx app we can render server-side state
+		// So we can debug it easier
+
+		// LwjglApplicationConfiguration config = new
+		// LwjglApplicationConfiguration();
+		// new LwjglApplication(new GameServer(), config);
+	}
+}
