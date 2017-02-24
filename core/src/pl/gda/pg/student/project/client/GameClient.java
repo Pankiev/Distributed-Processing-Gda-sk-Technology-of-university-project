80eee5b5ad9869f5235fb03c724e684b69f46433
Adrian Pankiewicz <farami11@tlen.pl>
2017-02-24 19:39:13 +0100

    Recommit as didn't stage all by a mistake

--- core/src/pl/gda/pg/student/project/client/GameClient.java
+++ core/src/pl/gda/pg/student/project/client/GameClient.java
@@ -1,33 +1,23 @@
-package pl.gda.pg.student.project;
+package pl.gda.pg.student.project.client;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class ProjectGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class GameClient extends ApplicationAdapter
+{
+	// TODO
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create()
+	{
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render()
+	{
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose()
+	{
 	}
 }
--- desktop/src/pl/gda/pg/student/project/desktop/ClientLauncher.java
+++ desktop/src/pl/gda/pg/student/project/desktop/ClientLauncher.java
@@ -2,11 +2,12 @@ package pl.gda.pg.student.project.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import pl.gda.pg.student.project.ProjectGame;
 
-public class DesktopLauncher {
+import pl.gda.pg.student.project.client.GameClient;
+
+public class ClientLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new ProjectGame(), config);
+		new LwjglApplication(new GameClient(), config);
 	}
 }
