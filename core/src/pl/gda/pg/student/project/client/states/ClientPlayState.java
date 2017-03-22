a84aca66cf1c5d2a2e28e6f41050c84db1f38bb9
janusz96 <bsmykowski@wp.pl>
2017-03-22 16:54:31 +0100

    server play state improvment

--- core/src/pl/gda/pg/student/project/client/states/ClientPlayState.java
+++ core/src/pl/gda/pg/student/project/client/states/ClientPlayState.java
@@ -1,65 +1,30 @@
 package pl.gda.pg.student.project.client.states;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.TreeMap;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.esotericsoftware.kryonet.Client;
-
 import pl.gda.pg.student.project.client.objects.Box;
 import pl.gda.pg.student.project.client.objects.Player;
-import pl.gda.pg.student.project.libgdxcommon.GameObjectsContainer;
 import pl.gda.pg.student.project.libgdxcommon.State;
-import pl.gda.pg.student.project.libgdxcommon.objects.GameObject;
 
-public class ClientPlayState extends State implements GameObjectsContainer
-{
-    private Map<Long, GameObject> gameObjects = Collections.synchronizedMap(new TreeMap<>());
+
+public class ClientPlayState extends State {
+
     private Player player = new Player(this);
-    private PlayInputHandler inputHandler = new PlayInputHandler(player);
-    private Client client;
+    private InputHandler inputHandler = new InputHandler(player);
+    private Box box = new Box(this);
 
-    public ClientPlayState(Client client)
-    {
-        this.client = client;
+
+    @Override
+    public void render(SpriteBatch batch) {
+        box.render(batch);
+        player.render(batch);
         Gdx.input.setInputProcessor(inputHandler);
-        player.setId(client.getID());
-        add(player);
-    }
-    
-    @Override
-    public void render(SpriteBatch batch)
-    {
-        for(GameObject object : gameObjects.values())
-            object.render(batch);
     }
 
     @Override
-    public void update()
-    {
-        for(GameObject object : gameObjects.values())
-            object.update();
+    public void update() {
         inputHandler.process();
     }
 
-    @Override
-    public void add(GameObject object)
-    {
-        gameObjects.put(object.getId(), object);
-    }
-
-    @Override
-    public void remove(GameObject object)
-    {
-        gameObjects.remove(object.getId());
-    }
-
-    @Override
-    public Map<Long, GameObject> getGameObjects()
-    {
-        return gameObjects;
-    }
 
 }
