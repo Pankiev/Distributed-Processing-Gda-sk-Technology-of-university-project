d7d51e208a8a0fe986f676482205bd3b3ec35b60
Adrian Pankiewicz <farami11@tlen.pl>
2017-03-24 12:48:18 +0100

    static Objects identifier class added

---
+++ core/src/pl/gda/pg/student/project/server/objects/ObjectsIdentifier.java
@@ -0,0 +1,21 @@
+package pl.gda.pg.student.project.server.objects;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class ObjectsIdentifier
+{
+    private static Map<Class<?>, String> identifiers = new TreeMap<>();
+    
+    static
+    {
+        identifiers.put(ServerPlayer.class, "Player");
+        identifiers.put(Wall.class, "Wall");
+    }
+    
+    public static String getObjectIdentifier(Class<?> type)
+    {
+        return identifiers.get(type);
+    }
+}
