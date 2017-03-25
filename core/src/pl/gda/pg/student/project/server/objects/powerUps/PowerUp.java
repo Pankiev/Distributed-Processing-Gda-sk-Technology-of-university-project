fdaa4550f8832e5413d92fe2b8f1243a7c490d1e
janusz96 <bsmykowski@wp.pl>
2017-03-25 14:09:28 +0100

    hanling power ups

--- core/src/pl/gda/pg/student/project/server/objects/powerUps/PowerUp.java
+++ core/src/pl/gda/pg/student/project/server/objects/powerUps/PowerUp.java
@@ -3,8 +3,11 @@ package pl.gda.pg.student.project.server.objects.powerUps;
 
 import com.badlogic.gdx.graphics.Texture;
 
+import pl.gda.pg.student.project.libgdxcommon.PacketsSender;
 import pl.gda.pg.student.project.libgdxcommon.State;
 import pl.gda.pg.student.project.libgdxcommon.objects.GameObject;
+import pl.gda.pg.student.project.packets.RemoveObjectInfo;
+import pl.gda.pg.student.project.server.objects.GameObjectsContainer;
 
 public abstract class PowerUp extends GameObject
 {
@@ -14,6 +17,12 @@ public abstract class PowerUp extends GameObject
         super(lookout, linkedState);
     }
 
-
+    public void deleteItself()
+    {
+        RemoveObjectInfo deleteItselfPacket = new RemoveObjectInfo();
+        deleteItselfPacket.id = this.getId();
+        ((PacketsSender) linkedState).send(deleteItselfPacket);
+        ((GameObjectsContainer) linkedState).remove(this.getId());
+    }
 
 }
