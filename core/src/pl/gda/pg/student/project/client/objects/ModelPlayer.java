b76e98a604ba0741704df81729275d326fa619bb
Adrian Pankiewicz <farami11@tlen.pl>
2017-03-25 10:40:38 +0100

    bmp to png fix

--- core/src/pl/gda/pg/student/project/client/objects/ModelPlayer.java
+++ core/src/pl/gda/pg/student/project/client/objects/ModelPlayer.java
@@ -7,10 +7,10 @@ import pl.gda.pg.student.project.client.GameClient;
 
 public class ModelPlayer extends ConnectionModelObject
 {
-    private Texture down = GameClient.assets.get("pacman_dol.bmp");
-    private Texture up = GameClient.assets.get("pacman_gora.bmp");
-    private Texture left = GameClient.assets.get("pacman_lewo.bmp");
-    private Texture right = GameClient.assets.get("pacman_prawo.bmp");
+	private Texture down = GameClient.assets.get("pacman_dol.png");
+	private Texture up = GameClient.assets.get("pacman_gora.png");
+	private Texture left = GameClient.assets.get("pacman_lewo.png");
+	private Texture right = GameClient.assets.get("pacman_prawo.png");
 
     public ModelPlayer()
     {
@@ -19,7 +19,7 @@ public class ModelPlayer extends ConnectionModelObject
 
     public ModelPlayer(Vector2 position)
     {
-        super(GameClient.assets.get("pacman_dol.bmp"), position);
+		super(GameClient.assets.get("pacman_dol.png"), position);
     }
 
     public void lookUp()
