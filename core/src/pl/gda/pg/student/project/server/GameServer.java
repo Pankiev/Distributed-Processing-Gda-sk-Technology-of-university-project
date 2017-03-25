e42bf7b01affcd4056e328565613b26efe19b30e
janusz96 <bsmykowski@wp.pl>
2017-03-25 11:27:08 +0100

    Bomb class added

--- core/src/pl/gda/pg/student/project/server/GameServer.java
+++ core/src/pl/gda/pg/student/project/server/GameServer.java
@@ -1,8 +1,5 @@
 package pl.gda.pg.student.project.server;
 
-import java.io.IOException;
-import java.util.Map;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -12,7 +9,6 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
-
 import pl.gda.pg.student.project.kryonetcommon.ConnectionSettings;
 import pl.gda.pg.student.project.kryonetcommon.IdSupplier;
 import pl.gda.pg.student.project.kryonetcommon.PacketsRegisterer;
@@ -25,15 +21,14 @@ import pl.gda.pg.student.project.packets.CreateObjectPacket;
 import pl.gda.pg.student.project.packets.DisconnectPacket;
 import pl.gda.pg.student.project.packets.PlayerPutBombPacket;
 import pl.gda.pg.student.project.packets.RemoveObjectInfo;
-import pl.gda.pg.student.project.packets.movement.ObjectMoveDownPacket;
-import pl.gda.pg.student.project.packets.movement.ObjectMoveLeftPacket;
-import pl.gda.pg.student.project.packets.movement.ObjectMoveRightPacket;
-import pl.gda.pg.student.project.packets.movement.ObjectMoveUpPacket;
-import pl.gda.pg.student.project.packets.movement.ObjectSetPositionPacket;
+import pl.gda.pg.student.project.packets.movement.*;
 import pl.gda.pg.student.project.server.helpers.PlayerPositioner;
 import pl.gda.pg.student.project.server.objects.ObjectsIdentifier;
 import pl.gda.pg.student.project.server.objects.ServerPlayer;
 import pl.gda.pg.student.project.server.states.ServerPlayState;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class GameServer extends ApplicationAdapter
 {
--- core/src/pl/gda/pg/student/project/server/objects/Bomb.java
+++ core/src/pl/gda/pg/student/project/server/objects/Bomb.java
@@ -1,6 +1,28 @@
 package pl.gda.pg.student.project.server.objects;
 
-public class Bomb
-{
+import com.badlogic.gdx.math.Vector2;
+import pl.gda.pg.student.project.libgdxcommon.State;
+import pl.gda.pg.student.project.libgdxcommon.objects.GameObject;
+import pl.gda.pg.student.project.server.GameServer;
 
+public class Bomb extends GameObject
+{
+    private ServerPlayer player;
+
+    public Bomb(State linkedState, Vector2 position, ServerPlayer serverPlayer) {
+        super(GameServer.assets.get("skrzynka.bmp"), linkedState);
+        super.setX(position.x);
+        super.setY(position.y);
+        this.player = serverPlayer;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public String getIdentifier() {
+        return ObjectsIdentifier.getObjectIdentifier(Bomb.class);
+    }
 }
