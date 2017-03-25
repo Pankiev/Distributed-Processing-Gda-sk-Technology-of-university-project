1136dcfc8416d97e5f4f6dcc20be51de7391fa8d
janusz96 <bsmykowski@wp.pl>
2017-03-25 11:43:01 +0100

    bmp to png in bomb

--- core/src/pl/gda/pg/student/project/client/objects/ModelBomb.java
+++ core/src/pl/gda/pg/student/project/client/objects/ModelBomb.java
@@ -8,7 +8,7 @@ public class ModelBomb extends ConnectionModelObject
 {
 	public ModelBomb(Vector2 position)
 	{
-		super(GameClient.assets.get("bomba1.bmp"), position);
+		super(GameClient.assets.get("bomba.png"), position);
 	}
 
 }
