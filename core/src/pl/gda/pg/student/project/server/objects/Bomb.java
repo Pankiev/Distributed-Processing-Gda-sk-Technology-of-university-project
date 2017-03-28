85b88cef496450757f6b2db1115bdec41e00fb63
Adrian Pankiewicz <farami11@tlen.pl>
2017-03-28 16:36:08 +0200

    Collision maker fixes

--- core/src/pl/gda/pg/student/project/server/objects/Bomb.java
+++ core/src/pl/gda/pg/student/project/server/objects/Bomb.java
@@ -14,95 +14,93 @@ import java.util.Map;
 
 public class Bomb extends GameObject
 {
-	private ServerPlayer player;
-	private boolean playerSteppedOutOfBomb = false;
-	private float timeToExplosion = 2.0f;
-	private List<Explosion> explosionChunks;
-	private boolean isAfterExplosion = false;
+    private ServerPlayer player;
+    private boolean playerSteppedOutOfBomb = false;
+    private float timeToExplosion = 2.0f;
+    private List<Explosion> explosionChunks;
+    private boolean isAfterExplosion = false;
 
-	public Bomb(State linkedState, Vector2 position, ServerPlayer serverPlayer)
-	{
-		super(GameServer.assets.get("bomba.png"), linkedState);
-		super.setX(position.x);
-		super.setY(position.y);
-		this.player = serverPlayer;
-	}
+    public Bomb(State linkedState, Vector2 position, ServerPlayer serverPlayer)
+    {
+        super(GameServer.assets.get("bomba.png"), linkedState);
+        super.setX(position.x);
+        super.setY(position.y);
+        this.player = serverPlayer;
+    }
 
-	@Override
-	public void update()
-	{
-		handlePlayerCollision();
-		handleTimer();
-	}
+    @Override
+    public void update()
+    {
+        handlePlayerCollision();
+        handleTimer();
+    }
 
-	private void handlePlayerCollision()
-	{
-		if (!this.isColliding(player))
-			playerSteppedOutOfBomb = true;
-	}
+    private void handlePlayerCollision()
+    {
+        if (!this.isColliding(player))
+            playerSteppedOutOfBomb = true;
+    }
 
-	private void handleTimer()
-	{
-		timeToExplosion -= Gdx.graphics.getDeltaTime();
-		if (timeToExplosion <= 0.0f)
-			explode();
-	}
+    private void handleTimer()
+    {
+        timeToExplosion -= Gdx.graphics.getDeltaTime();
+        if (timeToExplosion <= 0.0f)
+            explode();
+    }
 
-	public void explode()
-	{
-		isAfterExplosion = true;
-		Map<Long, GameObject> collisionObjects = ((GameObjectsContainer) linkedState).getGameObjects();
-		ExplosionMaker explosionMaker = new ExplosionMaker(
-				collisionObjects.values(), 
-				new Vector2(getX(), getY()),
-				linkedState);
+    public void explode()
+    {
+        isAfterExplosion = true;
+        Map<Long, GameObject> collisionObjects = ((GameObjectsContainer) linkedState).getGameObjects();
+        ExplosionMaker explosionMaker = new ExplosionMaker(collisionObjects.values(), new Vector2(getX(), getY()),
+                linkedState);
 
-		explosionChunks = explosionMaker.make(player.getRangeOfExplosion());
-		for (Explosion explosion : explosionChunks)
-		{
-			((GameObjectsContainer) linkedState).add(explosion);
-			sendExplosionCreationInfo(explosion);
-		}
+        explosionChunks = explosionMaker.make(player.getRangeOfExplosion());
+        for (Explosion explosion : explosionChunks)
+        {
+            ((GameObjectsContainer) linkedState).add(explosion);
+            sendExplosionCreationInfo(explosion);
+        }
 
-		Collection<GameObject> explosionColliders = explosionMaker.getExplosionColliders();
-		for (GameObject explosionCollider : explosionColliders)
-			handle(explosionCollider);
-		deleteItself();
-		player.bombExploded();
-	}
+        Collection<GameObject> explosionColliders = explosionMaker.getExplosionColliders();
+        for (GameObject explosionCollider : explosionColliders)
+            handle(explosionCollider);
+        deleteItself();
+        player.bombExploded();
+    }
 
-	private void handle(GameObject explosionCollider)
-	{
-		if (explosionCollider instanceof Box)
-			((Box) explosionCollider).destroyedByBomb();
-	}
+    private void handle(GameObject explosionCollider)
+    {
+        if (explosionCollider instanceof Box)
+            ((Box) explosionCollider).destroyedByBomb();
+    }
 
+    private void sendExplosionCreationInfo(Explosion explosion)
+    {
+        ExplosionCreatePacket createExplosion = new ExplosionCreatePacket();
+        createExplosion.textureName = explosion.getTextureName();
+        createExplosion.objectType = explosion.getIdentifier();
+        createExplosion.id = explosion.getId();
+        createExplosion.xPosition = explosion.getX();
+        createExplosion.yPosition = explosion.getY();
+        ((PacketsSender) linkedState).send(createExplosion);
+    }
 
-	private void sendExplosionCreationInfo(Explosion explosion)
-	{
-		ExplosionCreatePacket createExplosion = new ExplosionCreatePacket();
-		createExplosion.textureName = explosion.getTextureName();
-		createExplosion.objectType = explosion.getIdentifier();
-		createExplosion.id = explosion.getId();
-		createExplosion.xPosition = explosion.getX();
-		createExplosion.yPosition = explosion.getY();
-		((PacketsSender) linkedState).send(createExplosion);
-	}
+    @Override
+    public String getIdentifier()
+    {
+        return ObjectsIdentifier.getObjectIdentifier(Bomb.class);
+    }
 
-	@Override
-	public String getIdentifier()
-	{
-		return ObjectsIdentifier.getObjectIdentifier(Bomb.class);
-	}
+    @Override
+    protected boolean isCollideable()
+    {
+        return playerSteppedOutOfBomb;
+    }
 
-	@Override
-	protected boolean isCollideable()
-	{
-		return playerSteppedOutOfBomb;
-	}
-
-	public boolean isAfterExplosion() {
-		return isAfterExplosion;
-	}
+    public boolean isAfterExplosion()
+    {
+        return isAfterExplosion;
+    }
 
 }
